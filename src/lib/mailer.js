cb8501bb2129d793661f7bd61a08787f30e1db56
Muhammadib12 <160234764+Muhammadib12@users.noreply.github.com>
2025-04-11 18:59:18 +0300

    Update mailer.js

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail", // أو استخدم Mailtrap/Smtp2Go إذا كان Gmail لا يناسبك
+  service: "gmail", 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -12,8 +12,8 @@ const transporter = nodemailer.createTransport({
 
 export const sendLikeNotification = async (projectId, newLikes) => {
   const mailOptions = {
-    from: `"Braude Projects" <${process.env.EMAIL_USER}>`,
-    to: process.env.NOTIFY_EMAIL, // بريدك الشخصي الذي يصلك عليه التنبيه
+    from: `"Muhammad Portfolio" <${process.env.EMAIL_USER}>`,
+    to: process.env.NOTIFY_EMAIL, 
     subject: `👍 New Like on Project ${projectId}`,
     text: `Project ${projectId} just received a new like! Total likes: ${newLikes}`,
   };
